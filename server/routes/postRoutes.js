b0673f08626182618ba235564c9abd910a6bb0b2
Ahamed Saheer <saheer.ahamed570@gmail.com>
2023-01-31 18:08:47 +0530

    first commit

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import * as dotenv from 'dotenv'
+import { v2 as cloudinary } from 'cloudinary'
+
+import Post from '../models/post.js'
+
+dotenv.config()
+
+const router = express.Router()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+// GET ALL POSTS
+
+router.route('/').get(async (req, res) => {
+    try {
+        const posts = await Post.find({})
+
+        res.status(200).json({ success: true, data: posts })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+}).post(async (req, res) => {
+    try {
+        const { name, prompt, photo } = req.body
+        const photoURL = await cloudinary.uploader.upload(photo)
+
+        const newPost = await Post.create({
+            name,
+            prompt,
+            photo: photoURL.url
+        })
+
+        res.status(200).json({ success: true, data: newPost })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error })
+    }
+})
+
+// CREATE A POST
+
+
+
+export default router
